3d5f4388eb89314ce20b8ed5a1f062dea9925572
Kash <78519086+itkash@users.noreply.github.com>
2021-02-03 21:01:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-function compute() {
-    p = document.getElementById("principal").value;
-    if (p <= 0) {
+function compute()
+{
+    principal = document.getElementById("principal").value;
+
+    // warn if principal 0 or a negative number
+    if (principal <=0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
-        return false;
     }
 
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    var interest = +p * +r * +y / 100;
-    var total = +interest + +p;
-    var d = new Date();
-    var year = d.getFullYear();
-    var derivedyear = +year + +y;
+    else {
+    rate = document.getElementById("rate").value;
+    // parseInt turns string into integer
+    var years = parseInt(document.getElementById("years").value);
+    // works out interest
+    interest = principal*years*rate/100;
+    // gets todays date
+    var today = new Date();
+    // gets current year from todays date, and changes into integer
+    var year = parseInt(today.getFullYear());
+    var future_year = year+years;
 
-    var resultText = "If you deposit " + p + ",<br>"
-        + "at an interest rate of " + r + "%.<br>"
-        + "You will receive an amount of " + interest + ",<br>"
-        + "in the year " + derivedyear;
+    // returns HTML to the result
+    document.getElementById("result").innerHTML =
+    //<mark> highlights
+    "If you deposit: <mark>" + principal + "</mark><br>\
+    at an interest rate of: <mark>" + rate + "%</mark><br>\
+    You will receive an amount of <mark>" + interest + "</mark><br>\
+    in the year: <mark>" + future_year + "</mark><br>";
 
-    document.getElementById("result").innerHTML = resultText;
+    }
+    
 }
-
-function updateRange() {
-    r = document.getElementById("rate").value;
-    document.getElementById("rangeSpan").textContent = r + "%";
+        
+function slider() {
+    //updates slider rate
+    document.getElementById("result_slider").innerHTML = document.getElementById("rate").value + "%";
 }
